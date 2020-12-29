699e7373027be1d5bc6d8ad6dc47a64776f9b85a
Raul Herranz Munoz <raul.herranz@scmspain.com>
2020-12-29 11:04:57 +0100

    Initial commit

---
+++ src/main/kotlin/com/rhm/json2gpx/Json2gpxApplication.kt
@@ -0,0 +1,11 @@
+package com.rhm.json2gpx
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Json2gpxApplication
+
+fun main(args: Array<String>) {
+	runApplication<Json2gpxApplication>(*args)
+}
---
+++ src/test/kotlin/com/rhm/json2gpx/Json2gpxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rhm.json2gpx
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Json2gpxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
